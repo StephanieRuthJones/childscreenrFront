8219358b022d0a4141eb3bb28df1950a95b11f6b
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-04-02 14:04:02 -0600

    Added notes section to test items

--- childscreenr/src/components/languageareas/Receptive.js
+++ childscreenr/src/components/languageareas/Receptive.js
@@ -11,6 +11,7 @@ const Receptive = (props) => {
                 return <div className="p-2 bd-highlight" key={data.id}>{data.id}: {data.test_item}
                     <button type="button" className="btn btn-outline-success accuracy" id={data.id} value="1" onClick={(e) => props.responseAccuracyButton(e)}>1</button>
                     <button type="button" className="btn btn-outline-danger align-self-center accuracy" id={data.id} value="0" onClick={(e) => props.responseAccuracyButton(e)}>0</button>
+                    <input type="text" className="form-control form-control-sm notes" placeholder="Notes..." id={data.id}></input>
 
                     <div className="p-2 bd-highlight answer"><p>Correct: {data.correct_answer} </p><p>Incorrect: {data.incorrect_answer}</p></div>
                 </div>
--- childscreenr/src/components/screeningprep/Materials.js
+++ childscreenr/src/components/screeningprep/Materials.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Materials = () => {
     return (
-        <div className="button-group-vertical">
-            <button className="next btn btn-outline-success col-10 align-self-center" >Print Instructions</button>
-            <button className="next btn btn-outline-success col-10 align-self-center" >Print Stimuli</button>
-            <Link to="/studentinfo"><button className="next btn btn-outline-success col-10 align-self-center" >Enter Student Information</button></Link>
+        <div className="row">
+            <div className="button-group-vertical justify-content-center">
+                <button className="btn btn-outline-success col-10 prep" >Print Instructions</button>
+                <button className="btn btn-outline-success col-10 prep" >Print Stimuli</button>
+                <Link to="/studentinfo"><button className="btn btn-outline-success col-10 prep" >Enter Student Information</button></Link>
+            </div>
         </div>
 
     )
